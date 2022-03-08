1e26e0d8353dcfd4f84601bf838560d42c464848
Utkarsh Dhiman <utkarsh.dhiman@unthinkable.co>
2022-03-08 12:33:42 +0530

    exercise solved

--- src/index.js
+++ src/index.js
@@ -15,7 +15,8 @@
 // );
 
 import createStore from "./store/configureStore";
-import {bugAdded, bugRemoved, bugResolved, getUnrseolvedBugs} from "./store/bugs";
+import {bugAdded, bugRemoved, bugResolved, getUnrseolvedBugs, bugAssignedToUser} from "./store/bugs";
+import {userAdded, userRemoved} from "./store/users";
 import { projectAdded } from "./store/projects";
 
 
@@ -25,8 +26,10 @@ const store = createStore();
 store.dispatch(projectAdded({description: "desc 1"}));
 store.dispatch(projectAdded({description: "desc 1"}));
 store.dispatch(projectAdded({description: "desc 1"}));
+store.dispatch(userAdded({name: "user 1"}));
 store.dispatch(bugAdded({description: "desc 1"}));
 store.dispatch(bugAdded({description: "desc 2"}));
+store.dispatch(bugAssignedToUser({userId: 1, bugId: 2}));
 store.dispatch(bugResolved({id: 2}));
 // store.dispatch(bugRemoved({id: 1}));
 
--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import {createSelector} from "reselect";
 
 let lastId = 0;
 
@@ -21,9 +22,21 @@ const slice = createSlice({
       const index = bugs.findIndex((bug) => bug.id === action.payload.id);
       bugs[index].resolved = true;
     },
+    bugAssignedToUser: (bugs, action) => {
+      const {bugId, userId} = action.payload;
+
+      const index = bugs.findIndex(bug=>bug.id === bugId);
+      bugs[index].userId = userId;
+    }
   },
 });
 
 
-export const {bugAdded, bugRemoved, bugResolved} = slice.actions;
+export const {bugAdded, bugRemoved, bugResolved, bugAssignedToUser} = slice.actions;
 export default slice.reducer;
+
+
+export const getUnrseolvedBugs = createSelector(
+  state => state.entities.bugs,
+  bugs => bugs.filter(bug => !bug.resolved)
+);
